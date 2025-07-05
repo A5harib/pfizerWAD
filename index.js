d57da9e452230dc679e3293c101d159e5b013f31
A5harib <asharibhashmi7@gmail.com>
2025-07-05 05:25:55 +0500

    a

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Home Page Route
 // This route serves the index.html file located in the views directory
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
 app.get("/secure-route", IsSecureUrl, (req, res) => {
